de31120a01fd0fe2b3639c6d32786b5815c137e9
auwaanet <auwaanet@gmail.com>
2022-09-26 14:48:23 +0700

    edit Key key -> Key? key , this.onNext -> required this.onNext

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,182 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/avd.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:flutter_onboard_screen/widgets/animated_indicator.dart';
+
+const blue = Color(0xFF4781ff);
+const kTitleStyle = TextStyle(
+    fontSize: 30, color: Color(0xFF01002f), fontWeight: FontWeight.bold);
+const kSubtitleStyle = TextStyle(fontSize: 22, color: Color(0xFF88869f));
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Demo Onboard',
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  PageController pageController = new PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+            child: PageView(
+                controller: pageController,
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+              Slide(
+                  hero: Image.asset("./assets/hero-1.png"),
+                  title: "Boost your traffic",
+                  subtitle:
+                      "Outreach to many social networks to improve your statistics",
+                  onNext: nextPage),
+              Slide(
+                  hero: Image.asset("./assets/hero-2.png"),
+                  title: "Give the best solution",
+                  subtitle:
+                      "We will give best solution for your business isues",
+                  onNext: nextPage),
+              Slide(
+                  hero: Image.asset("./assets/hero-3.png"),
+                  title: "Reach the target",
+                  subtitle:
+                      "With our help, it will be easier to achieve your goals",
+                  onNext: nextPage),
+              Scaffold(
+                backgroundColor: Colors.white,
+                body: Center(
+                  child: Text(
+                    'Be kind to yourself',
+                    style: kTitleStyle,
+                  ),
+                ),
+              )
+            ])),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+
+  void nextPage() {
+    pageController.nextPage(
+        duration: const Duration(milliseconds: 200), curve: Curves.ease);
+  }
+}
+
+class Slide extends StatelessWidget {
+  final Widget hero;
+  final String title;
+  final String subtitle;
+  final VoidCallback onNext;
+
+  const Slide(
+      {Key? key,
+      required this.hero,
+      required this.title,
+      required this.subtitle,
+      required this.onNext})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(child: hero),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text(
+                  title,
+                  style: kTitleStyle,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  subtitle,
+                  style: kSubtitleStyle,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                ProgressButton(onNext: onNext),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          GestureDetector(
+            onTap: onNext,
+            child: Text(
+              "Skip",
+              style: kSubtitleStyle,
             ),
-          ],
+          ),
+          SizedBox(
+            height: 4,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProgressButton extends StatelessWidget {
+  final VoidCallback onNext;
+  const ProgressButton({Key? key, required this.onNext}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 75,
+      height: 75,
+      child: Stack(children: [
+        AnimatedIndicator(
+          duration: const Duration(seconds: 10),
+          size: 75,
+          callback: onNext,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Center(
+          child: GestureDetector(
+            child: Container(
+              height: 60,
+              width: 60,
+              child: Center(
+                child: SvgPicture.asset(
+                  "./assets/arrow.svg",
+                  width: 10,
+                ),
+              ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(99), color: blue),
+            ),
+            onTap: onNext,
+          ),
+        )
+      ]),
     );
   }
 }
